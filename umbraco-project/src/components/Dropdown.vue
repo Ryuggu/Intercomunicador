e3a6664cfc8962eb1b73435ff1c063f414090391
hawar <hawar-sevar@hotmail.com>
2020-04-17 12:22:36 +0200

    Header
    Header NavBar

---
+++ umbraco-project/src/components/Dropdown.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="menu-item" @click="isOpen =!isOpen">
+    <a href="#">
+        {{title}}<a href="#"><vue-fontawesome icon="cog" size="2" color="lightgray"></vue-fontawesome></a>
+
+        </a>
+        <transition name="fade" apper>
+        <div class="sub-menu" v-if="isOpen">
+            <div v-for="(item, i) in items" :key="i" class="menu-item">
+            <a :href="item.link">{{item.title}}</a>
+        </div>
+     </div>
+        </transition>
+ </div>
+</template>
+<script>
+export default {
+    name:'Dropdown',
+    props:['title','items'],
+    data(){
+    return{
+        isOpen:false
+    }
+    }
+}
+</script>
+<style scoped>
+nav .menu-item .sub-menu{
+    position: absolute;
+    background-color:#F6EEE7;
+    border-radius: 16px 16px 16px 16px; 
+    padding: 25px 15px;
+    top:100%;
+    left:10%;
+    inline-size: max-content;
+    color:inherit;
+    
+}
+nav .menu-item .sub-menu a{
+    color:black;
+    padding:35px 5px;
+    text-decoration: none;
+    }
+.fade-enter-activ,
+.fade-leave-active{
+    transition: all.4s ease-out;
+
+}
+.fade-enter,
+.fade-enter-to{
+    opacity: 0;
+}
+
+
+
+</style>
---
+++ umbraco-project/src/components/NavBar.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="header__title">
+    <h1>Rastreador de Estadísticas para Entradas de Intercomunicador</h1>
+        
+    <nav>
+   <div class="menu-item">
+       <Dropdown title=""  :items="Setting" /> 
+       <a href="#"><vue-fontawesome icon="user" size="2" color="lightgray"></vue-fontawesome></a>
+       </div>
+    </nav>
+  </div>
+</template>
+<script>
+import Dropdown from "./Dropdown";
+
+export default {
+    name: 'NavBar',
+    components:{Dropdown},
+    data(){
+        return{
+            Setting:[
+                {
+                    title:'CHange Theme',
+                    link:'#'
+                },
+                {
+                    title:'Tickets per page',
+                    link:'#'
+                }
+            ]
+        }
+    }
+    
+}
+</script>
+<style scoped>
+.header__site{
+    height: 20% ;
+}
+.header__title{
+    text-align: center;
+    padding-top: 25px;
+
+}
+
+h1{
+    color: white;
+    font-size: 22px;
+}
+nav{
+    display: flex;
+    align-items:center;
+    justify-content:flex-end;
+}
+
+.menu-item a.active{
+    background-color: rgb(55, 65, 100);
+
+
+}
+
+.menu-item{
+    color: azure;
+    padding: 2px 38px;
+    position: relative;
+    display: flex;
+}  
+.menu-item a{
+    color: inherit;
+    text-decoration: none;
+}  
+.menu-sub-title{color: white;
+
+}
+
+
+</style>
