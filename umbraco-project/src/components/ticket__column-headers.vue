4240c43d05f45a0e504f0a0232516f479ea63359
Darek <das@umbraco.dk>
2020-04-18 23:32:11 +0200

    components initial commit

---
+++ umbraco-project/src/components/ticket__column-headers.vue
@@ -0,0 +1,64 @@
+<template>
+  
+    <div class="column-headers">
+        <div class="ticket-header light-blue">
+            <p class="ticket-header__text">
+                Creation time
+            </p>
+        </div>
+        <div class="ticket-header blue">
+            <p class="ticket-header__text">
+                First response
+            </p>
+        </div>
+        <div class="ticket-header light-blue">
+            <p class="ticket-header__text">
+                Handling time
+            </p>
+        </div>
+        <div class="ticket-header blue">
+            <p class="ticket-header__text">
+                Office hours?
+            </p>
+        </div>
+        <div class="ticket-header light-blue">
+            <p class="ticket-header__text">
+                Conversation ID
+            </p>
+        </div>
+        <div class="ticket-header blue">
+            <p class="ticket-header__text">
+                Location
+            </p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/*ticket__column-headers*/
+
+.ticket-header {
+    display: flex; 
+    flex: 1;
+    font-weight: 700;
+    height: 48px;
+    margin-right: 8px;
+    margin-top: 20px;
+    box-shadow: 3px 3px 6px 0px rgba(0,0,0,0.75);
+}
+
+.ticket-header:last-of-type { /*no margin on the last element*/
+    margin-right: 0;
+}
+
+.ticket-header__text {
+    margin: auto 20px; /* centers the text in the div + the display: flex on the parent */ 
+}
+</style>
