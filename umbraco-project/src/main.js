172a7e3018da409d039d6c1038c5375edfdfc1d2
lrpe <lassep@protonmail.com>
2020-04-19 14:09:49 +0200

    Filter component

--- umbraco-project/src/main.js
+++ umbraco-project/src/main.js
@@ -3,6 +3,27 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { Datetime } from "vue-datetime";
+import "vue-datetime/dist/vue-datetime.css";
+Vue.component("Datetime", Datetime);
+
+Vue.directive("click-outside", {
+  bind(el, binding, vnode) {
+    var vm = vnode.context;
+    var callback = binding.value;
+
+    el.clickOutsideEvent = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        return callback.call(vm, event);
+      }
+    };
+    document.body.addEventListener("click", el.clickOutsideEvent);
+  },
+  unbind(el) {
+    document.body.removeEventListener("click", el.clickOutsideEvent);
+  }
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
