0326b437306262188d8ee1654208a53cb4e9d54f
lrpe <lassep@protonmail.com>
2020-05-09 11:28:59 +0200

    removed duplicate vue instance

--- umbraco-project/src/main.js
+++ umbraco-project/src/main.js
@@ -5,31 +5,6 @@ import store from './store';
 import axios from 'axios';
 import "font-awesome/css/font-awesome.min.css";
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  created() {
-    const userString = localStorage.getItem('user') // grab user data from local storage
-    if (userString) { // check to see if there is indeed a user
-      const userData = JSON.parse(userString) // parse user data into JSON
-      this.$store.commit('SET_USER_DATA', userData) // restore user data with Vuex
-    }
-
-    axios.interceptors.response.use(
-      response => response, // simply return the response 
-      error => {
-        if (error.response.status === 401) { // if we catch a 401 error
-          this.$store.dispatch('logout') // force a log out 
-        }
-        return Promise.reject(error) // reject the Promise, with the error as the reason
-      }
-    )
-  },
-  render: h => h(App)
-}).$mount('#app')
-
 
 /*CSS*/
 import "@/assets/css/main.css";
